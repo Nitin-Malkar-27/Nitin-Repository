023045c2785669829f2af19aa047ab07510eaa83
Nitin-Malkar-27 <131537111+Nitin-Malkar-27@users.noreply.github.com>
2023-10-06 16:37:03 +0530

    Add files via upload

---
+++ BootMVCProj07-MiniProject-01-CURDOperations/src/main/java/com/nt/service/IEmployeeMgmtService.java
@@ -0,0 +1,16 @@
+package com.nt.service;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.nt.model.Employee;
+
+@Service
+public interface IEmployeeMgmtService {
+       public  List<Employee>  getAllEmployees();
+       public  String  registerEmployee(Employee emp); 
+       public  Employee   getEmployeeByNo(int no);
+       public  String   editEmployee(Employee emp);
+       public  String   deleteEmployee(int no);
+}
